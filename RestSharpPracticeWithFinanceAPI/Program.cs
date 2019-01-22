25ef99ecdd1db4bf05905832475db769c2be3dcf
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-21 22:04:45 -0500

    Db connection and update successful

--- RestSharpPracticeWithFinanceAPI/Program.cs
+++ RestSharpPracticeWithFinanceAPI/Program.cs
@@ -2,6 +2,7 @@
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,14 @@ namespace RestSharpPracticeWithFinanceAPI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a symbol");
+            var symbol = Console.ReadLine();
             string key = ApiKey.apiKey;
             Console.WriteLine("key: " + key);
             RestClient client = new RestClient("https://www.alphavantage.co");
 
             // create a new request
-            var request = new RestRequest("/query?function=GLOBAL_QUOTE&symbol=googl&apikey=1PO9VNXTLC4Z3GJR" + key);
+            var request = new RestRequest("/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=1PO9VNXTLC4Z3GJR" + key);
             Console.WriteLine("request: " + request);
 
             // execute the request
@@ -31,8 +34,9 @@ namespace RestSharpPracticeWithFinanceAPI
             StockQuote stockQuote = JsonConvert.DeserializeObject<StockQuote>(finalContent);
 
             stockQuote.parseOutput();
-            
+
             Console.WriteLine(stockQuote.symbol);
+            Console.WriteLine(stockQuote.open);
             Console.WriteLine(stockQuote.high);
             Console.WriteLine(stockQuote.low);
             Console.WriteLine(stockQuote.price);
@@ -41,6 +45,30 @@ namespace RestSharpPracticeWithFinanceAPI
             Console.WriteLine(stockQuote.previousClose);
             Console.WriteLine(stockQuote.change);
             Console.WriteLine(stockQuote.changePercent);
+
+            // connect to the db
+            var connstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RestSharpPracticeWithFinanceAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connstring))
+            {
+                connection.Open();
+
+                SqlCommand insCommand = new SqlCommand("INSERT INTO [StockQuote] (symbol, \"open\", high, low, price, volume, latestTradingDay, previousClose, change, changePercent) VALUES (@symbol, @open, @high, @low, @price, @volume, @latestTradingDay, @previousClose, @change, @changePercent)", connection);
+
+                insCommand.Parameters.AddWithValue("@symbol", stockQuote.symbol);
+                insCommand.Parameters.AddWithValue("@open", stockQuote.open);
+                insCommand.Parameters.AddWithValue("@high", stockQuote.high);
+                insCommand.Parameters.AddWithValue("@low", stockQuote.low);
+                insCommand.Parameters.AddWithValue("@price", stockQuote.price);
+                insCommand.Parameters.AddWithValue("@volume", stockQuote.volume);
+                insCommand.Parameters.AddWithValue("@latestTradingDay", stockQuote.latestTradingDay);
+                insCommand.Parameters.AddWithValue("@previousClose", stockQuote.previousClose);
+                insCommand.Parameters.AddWithValue("@change", stockQuote.change);
+                insCommand.Parameters.AddWithValue("@changePercent", stockQuote.changePercent);
+
+                insCommand.ExecuteNonQuery();
+                Console.WriteLine("DB updated");
+                connection.Close();
+            }
         }
     }
 }
